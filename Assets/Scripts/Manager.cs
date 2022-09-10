0456389d302ae82d7579ed3235b04b9a4c301f8d
alperTwoShot <alper2meydan@gmail.com>
2022-09-10 13:25:09 +0300

    Enemy 2 spawn point

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,8 +6,14 @@ public class Manager : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
     [SerializeField] Transform spawnPos;
+    [SerializeField] Transform spawnPos1;
+    bool right = true;
 
     public void Spawn(){
+        if(right)
         Instantiate(enemy, spawnPos.position, Quaternion.identity);
+        else Instantiate(enemy, spawnPos1.position, Quaternion.identity);
+        right = !right;
     }
+
 }
